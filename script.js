39b21e7f4c7fd6fe59b53f4dae0bd6315dac564c
Maksat Ylyasov <maksatylyasov@gmail.com>
2022-05-04 06:35:19 +0000

    changes made from theia lab on May 4

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+
+
 function compute()
 {
-    p = document.getElementById("principal").value;
+    //Set up Variables
+    var principal = document.getElementById("principal").value;
+        //check if the amount more than 0
+    if(principal<=0){
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }
+
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+
+    //Displays the final interest rate
+    if(principal>0)
+        document.getElementById("result").innerHTML="If you deposit <highlight>"+principal+"</highlight>,\<br\>at an interest rate of <highlight>"+rate+"%</highlight>\<br\>You will receive an amount of <highlight>"+interest+"</highlight>,\<br\>in the year <highlight>"+year+"</highlight>\<br\>";
     
+
+
 }
         
